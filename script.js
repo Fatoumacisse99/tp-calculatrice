9a2206d8f2a2ee9cf7ae757afba2372b4ab4639d
Fatoumacisse99 <cfama673@gmail.com>
2024-04-27 16:35:41 +0200

    j'ai apporter des modification au niveau de java script

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function calculate(operation) {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
+    
+    let num3 = parseFloat(document.getElementById("num3").value);
     let resultElement = document.getElementById("result");
-
     let result;
     if (operation === "+") {
+        if (!isNaN(num3)) {
+        result = num1 + num2 + num3;
+    } else {
         result = num1 + num2;
+    }
     } else if (operation === "-") {
         result = num1 - num2;
     } else if (operation === "*") {
-        result = num1 * num2;
+        if (!isNaN(num3)) {
+            result = num1 * num2 * num3;
+        } else {
+            result = num1 * num2;
+        }
     } else if (operation === "/") {
         if (num2 === 0) {
             result = "Division par zéro impossible";
